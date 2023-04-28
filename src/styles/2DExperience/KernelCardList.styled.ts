b3f83058b80d57f4aa09463e1f82de556b9eb3e3
Le Trung <trungle30902@gmail.com>
2023-04-29 00:39:48 +0700

    fix: hidden bug

---
+++ src/styles/2DExperience/KernelCardList.styled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: auto;
+  top: 0;
+  left: 0;
+`
+
+export const StyledKernelCardListWrapper = styled.div`
+  padding: 15vh 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 5em 2em;
+  overflow: auto;
+`
+
+export const StyledKernelCardWrapper = styled.div`
+
+`
